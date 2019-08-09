0ec34765670a9473a6c89f287c7a4e1f0eb04ce3
Aiden Gleave <aidengleave@gmail.com>
2019-08-09 14:22:19 +0100

    Test: Parcel knos what size it is

--- spec/parcel.spec.js
+++ spec/parcel.spec.js
@@ -6,4 +6,7 @@ describe(Parcel, () => {
   it('an instance of parcel knows its own dimensions', () => {
     expect(parcel.dimensions).toEqual([1, 2, 3]);
   });
+  it('an instance of parcel knows what size it is', () => {
+    expect(parcel.size).toEqual('small');
+  });
 });
